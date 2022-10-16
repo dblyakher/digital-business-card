499d97291e7cbec423acc1294216a4cf173c1f08
dblyakher <101061863+dblyakher@users.noreply.github.com>
2022-10-16 17:47:26 -0400

    Add files via upload

---
+++ Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="headshot" src="./Images/headshot_2.png" />       
+        </div>
+    )
+}
+
+export default Header
